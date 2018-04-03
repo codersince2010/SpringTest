0b35c526a53b65e2008d5d83ce7e8b88919a29e5
Mohit <mohbhatia@Mohits-MacBook-Pro.local>
2018-04-02 21:38:04 -0500

    First commit

---
+++ spring-boot-web-jsp/src/main/java/com/coder2010/SpringBootWebApplication.java
@@ -0,0 +1,17 @@
+package com.coder2010;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+    
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SpringBootWebApplication.class, args);
+        
+
+}
+}
